432cd985c060f1b8e1f8b7fecd31e84ce6f76b5e
serjious <dasaewn4@gmail.com>
2024-11-22 05:14:50 +0300

    Add vehicle subclasses: Car, Truck, Motocycle

--- main.py
+++ main.py
@@ -24,17 +24,66 @@ class Vehicle:
 			"model":	  self.__model,
 			"year":		  self.__year
 		}
+
 	def from_dict(self, data):
 		self.__vehicle_id = data['vehicle_id'] 
 		self.__make = data['make']
 		self.__model = data['model'] 
 		self.__year = data['year'] 
 
+class Car(Vehicle):
+	def __init__(self, vehicle_id: int, make: str,
+				 model: str, year: int, doors: int):
+		Vehicle.__init__(self, vehicle_id, make, model, year)
+		self.__doors = doors
+
+	def to_dict(self):
+		data = Vehicle.to_dict(self)
+		data['doors'] = self.__doors
+		return data
+	
+	def from_dict(self, data):
+		Vehicle.from_dict(self, data)
+		self.__doors = data['doors']
+		
+class Truck(Vehicle):
+	def __init__(self, vehicle_id: int, make: str,
+				 model: str, year: int, capacity: float):
+		Vehicle.__init__(self, vehicle_id, make, model, year)
+		self.__capacity = capacity 
+
+	def to_dict(self):
+		data = Vehicle.to_dict(self)
+		data['capacity'] = self.__capacity
+		return data
+	
+	def from_dict(self, data):
+		Vehicle.from_dict(self, data)
+		self.__capacity= data['capacity']
+
+class Motocycle(Vehicle):
+	def __init__(self, vehicle_id: int, make: str,
+				 model: str, year: int, type_moto: str):
+		Vehicle.__init__(self, vehicle_id, make, model, year)
+		self.__type_moto = type_moto
+
+	def to_dict(self):
+		data = Vehicle.to_dict(self)
+		data['type_moto'] = self.__type_moto
+		return data
+	
+	def from_dict(self, data):
+		Vehicle.from_dict(self, data)
+		self.__type_moto = data['type_moto']
+
+
 def main():
-	gfg = Vehicle(1, "BMW", "X5", 2015)
-	d = gfg.to_dict()
-	ddd = Vehicle(d['vehicle_id'], d['make'], d['model'], d['year'])
-	print(ddd.to_dict())
+	car = Car(1, "BMW", "X5", 2015, 4)
+	truck = Truck(2,  "Scania", "hz", 2010, 200000)
+	moto = Motocycle(3, "Honda", "hz2", 2020, 'sportbike')
+	print(car.to_dict())
+	print(truck.to_dict())
+	print(moto.to_dict())
 
 
 
