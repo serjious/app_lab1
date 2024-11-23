75ceb26ef58fc58302197bbae64c0d6ea7a6bb62
serjious <dasaewn4@gmail.com>
2024-11-23 08:35:51 +0300

    Add class bus, Ticket, Passanger

--- main.py
+++ main.py
@@ -21,10 +21,25 @@ class IODataBaseError(TusovaError):
 		super().__init__(message)
 
 
+class DriveLicense:
+	def __init__(self, driver_license: str):
+		if driver_license in "ABC":
+			self._driver_license = driver_license
+		else:
+			self._driver_license = "None"
+
+
+class Ticket:
+	def __init__(self, ticket_id: int):
+		if ticket_id < 0:
+			raise ValueError('ticket_id должен быть больше 0')
+		self._ticket_id = ticket_id
+
+
 class Person:
 	def __init__(self, name: str = "", age: int = 0):
 		if not isinstance(name, str):
-			raise TypeError(f'Некорректный тип {name.type()}')
+			raise TypeError('Некорректный тип name')
 		if not isinstance(age, int) or age < 0:
 			raise ValueError('Возраст должен быть больше 0')
 		self.__name = name
@@ -40,27 +55,42 @@ class Person:
 		self.__name = data['name']
 		self.__age = data['age']
 
-	
-class Driver(Person):
-	def __init__(self, name: str = "", age: int = 0, drive_license: str = ""):
-		Person.__init__(self, name, age)
-		if not isinstance(drive_license, str) :
-			raise TypeError(f'Некорректный тип {drive_license.type()}')
-		self.__drive_license = drive_license
 
+class Passenger(Person, Ticket):
+	def __init__(self, name: str = "", age: int = 0, ticket_id: int = 0):
+		Ticket.__init__(self, ticket_id)
+		Person.__init__(self, name, age)
+	
 	def to_dict(self):
 		data = Person.to_dict(self)
-		data['drive_license'] = self.__drive_license
+		data['ticket'] = self._ticket_id
 		return data
 
 	def from_dict(self, data):
 		Person.from_dict(self, data)
-		self.__drive_license = data['drive_license']
+		self._ticket_id = data['ticket']
+	
+
+class Driver(Person, DriveLicense):
+	def __init__(self, name: str = "", age: int = 0, drive_license: str = ""):
+		DriveLicense.__init__(self, drive_license)
+		Person.__init__(self, name, age)
+		if not isinstance(drive_license, str):
+			raise TypeError('Некорректный тип drive_license}')
+
+	def to_dict(self):
+		data = Person.to_dict(self)
+		data['drive_license'] = self._driver_license
+		return data
+
+	def from_dict(self, data):
+		Person.from_dict(self, data)
+		self._driver_license = data['drive_license']
 			
 
 class Engine:
 	def __init__(self, power: int = 0):
-		if not isinstance(power, int) or power < 0:
+		if power < 0:
 			raise ValueError('Мощность двигателя должна быть больше 0 л.с.')
 		self.__power = power
 
@@ -74,8 +104,12 @@ class Engine:
 
 class Vehicle:
 	def __init__(self, driver: Driver = Driver(), engine: Engine = Engine(), 
-				 vehicle_id: int = 0, make: str = "", 
-				 model: str = None, year: int = 0):
+			vehicle_id: int = 0, make: str = "",
+			model: str = None, year: int = 0):
+		if not isinstance(driver, Driver):
+			raise TypeError('Некорректный тип driver}')
+		if not isinstance(engine, Engine):
+			raise TypeError('Некорректный тип engine}')
 		self.__driver = driver
 		self.__engine = engine
 		self.__vehicle_id = vehicle_id
@@ -105,10 +139,14 @@ class Vehicle:
 
 class Car(Vehicle):
 	def __init__(self, driver: Driver = Driver(), engine: Engine = Engine(),
-				 vehicle_id: int = 0, make: str = "",
-				 model: str = "", year: int = 0, doors: int = 0):
+			vehicle_id: int = 0, make: str = "",
+			model: str = "", year: int = 0, doors: int = 0):
 		Vehicle.__init__(self, driver, engine,
 				 vehicle_id, make, model, year)
+		if not isinstance(driver, Driver):
+			raise TypeError('Некорректный тип driver}')
+		if not isinstance(engine, Engine):
+			raise TypeError('Некорректный тип engine}')
 		self.__doors = doors
 
 	def to_dict(self):
@@ -123,10 +161,10 @@ class Car(Vehicle):
 
 class Truck(Vehicle):
 	def __init__(self, driver: Driver = Driver(), engine: Engine = Engine(),
-				 vehicle_id: int = 0, make: str = "",
-				 model: str = "", year: int = 0, capacity: float = 0):
+			vehicle_id: int = 0, make: str = "",
+			model: str = "", year: int = 0, capacity: float = 0):
 		Vehicle.__init__(self, driver, engine,
-				 vehicle_id, make, model, year)
+			 vehicle_id, make, model, year)
 		self.__capacity = capacity 
 
 	def to_dict(self):
@@ -141,10 +179,10 @@ class Truck(Vehicle):
 
 class Motocycle(Vehicle):
 	def __init__(self, driver: Driver = None, engine: Engine = None,
-				 vehicle_id: int = 0, make: str = "",
-				 model: str = "", year: int = 0, type_moto: str = ""):
+			vehicle_id: int = 0, make: str = "",
+			model: str = "", year: int = 0, type_moto: str = ""):
 		Vehicle.__init__(self, driver, engine,
-				 vehicle_id, make, model, year)
+			 vehicle_id, make, model, year)
 		self.__type_moto = type_moto
 
 	def to_dict(self):
@@ -155,6 +193,24 @@ class Motocycle(Vehicle):
 	def from_dict(self, data):
 		Vehicle.from_dict(self, data)
 		self.__type_moto = data['type_moto']
+
+
+class Bus(Vehicle):
+	def __init__(self, driver: Driver = None, engine: Engine = None,
+			vehicle_id: int = 0, make: str = "",
+			model: str = "", year: int = 0, passangers: list = []):
+		Vehicle.__init__(self, driver, engine,
+			 vehicle_id, make, model, year)
+		self.__passangers = passangers.copy()
+
+	def to_dict(self):
+		data = Vehicle.to_dict(self)
+		z = self.__passangers[0].to_dict()
+		print(z)
+		return data
+	
+	def from_dict(self, data):
+		Vehicle.from_dict(self, data)
 
 
 class File:
@@ -176,6 +232,8 @@ class File:
 
 class JSON_File(File):
 	def __init__(self, filename: str, read: bool):
+		if not filename:
+			raise ValueError(f"Невозможно создать файл с именем {filename}")
 		if not (filename[-5:] == '.json'):
 			filename += '.json'
 		File.__init__(self, filename, read)
@@ -183,6 +241,8 @@ class JSON_File(File):
 		
 class XML_File(File):
 	def __init__(self, filename: str, read: bool):
+		if not filename:
+			raise ValueError(f"Невозможно создать файл с именем {filename}")
 		if not (filename[-4:] == '.xml'):
 			filename += '.xml'
 		File.__init__(self, filename, read)
@@ -220,7 +280,8 @@ class VehicleDatabase():
 		self.__json_file = JSON_File(self.__filename, read=False).file()
 		data = {vehicle_id: vehicle.to_dict()
 			for vehicle_id, vehicle in self.__vehicles.items()}
-		json.dump(data, self.__json_file, indent=4)
+		print(data)
+		#json.dump(data, self.__json_file, indent=4)
 		self.__json_file.close()
 	
 	def from_json(self, obj: 'VehicleDatabase'):
@@ -245,28 +306,35 @@ class VehicleDatabase():
 def main():
 	db = VehicleDatabase("aboba")
 	gg = VehicleDatabase("copy")
+	pass1 = Passenger("Dima", 19, 1)
+	pass2 = Passenger("Vova", 19, 2)
+	pass3 = Passenger("Lesha", 19, 3)
 	en_car = Engine(300)
 	en_moto = Engine(350)
 	en_truck = Engine(900)
+	en_bus = Engine(600)
 	driver_car = Driver("Grisha", 19, "A")
-	driver_car2 = Driver("Aziz", 18, "B")
+	driver_car2 = Driver("Aziz", 18, "A")
 	driver_truck = Driver("Misha", 19, "C")
-	driver_moto = Driver("Nikita", 20, "D")
+	driver_moto = Driver("Nikita", 20, "B")
+	driver_bus = Driver("Igor", 19, "С")
 	car = Car(driver_car, en_car, 1, "BMW", "X5", 2015, 4)
-	car2 = Car(driver_car2, en_car, 4, "AUDI", "fdg", 2002, 4)
+	car2 = Car(driver_car2, en_car, 5, "AUDI", "fdg", 2002, 4)
 	truck = Truck(driver_truck, en_truck, 2, "Scania", "hz", 2010, 200000)
 	moto = Motocycle(driver_moto, en_moto, 3, "Honda", "hz2", 2020, 'sportbike')
+	bus = Bus(driver_bus, en_bus, 4, "kamaz", "dw", 2017, [pass1, pass2, pass3])
 	
 	db.add_vehicle(car)
 	db.add_vehicle(truck)
 	db.add_vehicle(moto)
+	db.add_vehicle(bus)
+	#db.to_json()
+	#db.update_vehicle(3, car2)
+	#db.to_json()
+	#db.delete_vehicle(3)
 	db.to_json()
-	db.update_vehicle(3, car2)
-	db.to_json()
-	db.delete_vehicle(3)
-	db.to_json()
-	gg.from_json(db)
-	gg.to_json()
+	#gg.from_json(db)
+	#gg.to_json()
 
 main()
 '''
